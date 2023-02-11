3fd5e7a1ebde90f0722822e0e530ecba57e3bb63
Aziat <abdimalikovaziat@gmail.com>
2023-02-11 10:09:41 +0600

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/products/" element={<ProductPage />} />
+        <Route path="/products" element={<ProductPage />} />
 
         <Route path="/products/:productId" element={<ProductDetail />} />
         <Route path="/my-card" element={<MyCard />} />
